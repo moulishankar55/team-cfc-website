b4ff8ec7e50c4a31200a4a5aa766421f205fe384
Mouli Shankar <moulidarling95@gmail.com>
2026-09-02 22:12:32 +0530

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -42,17 +42,11 @@ document.addEventListener("DOMContentLoaded", () => {
     "IMG_8917.JPG",
     "IMG_8920.JPG",
     "IMG_9143.JPG",
-    "IMG-20240811-WA0022_1.jpg",
-    "IMG-20240827-WA0001.jpg",
     "IMG-20240828-WA0001.jpg",
     "IMG-20240907-WA0027.jpg",
     "IMG-20240907-WA0033.jpg",
     "IMG-20240907-WA0039.jpg",
-    "IMG-20250812-WA0023 (1).jpg",
-    "IMG-20250812-WA0023.jpg",
     "IMG-20250825-WA0040 (1).jpg",
-    "IMG_20250827_115633 (1).jpg",
-    "IMG_20250827_115635 (1).jpg",
     "Screenshot 2026-08-30 150651.png"
         ],
 
